4cfc7965156167c727700f32eabd39f6c738018d
Sahil Narula <sahilnarula254@gmail.com>
2024-07-30 14:41:46 +0530

    Create background.py

---
+++ background.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+# Set the background image
+page_bg_img = '''
+<style>
+body {
+background-image: url("data/your_image.jpg");
+background-size: cover;
+}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# Streamlit app content
+st.title('Online FIR System')
+# Additional content
